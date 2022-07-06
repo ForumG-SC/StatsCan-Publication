7fa6a6b3d31d315aaf1357761dc979ea722b6d9a
Forum Gorawala <84411852+ForumG-SC@users.noreply.github.com>
2022-07-06 14:17:56 -0400

    he

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>CI/CD Demo!</Text>
+      <Text>Deployment Demo!!</Text>
       <StatusBar style="auto" />
     </View>
   );
